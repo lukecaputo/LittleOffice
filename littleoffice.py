1fb17a66544c496765f6a5aba3794f3b6b26b42c
Luke Caputo <mail@lukecaputo.com>
2017-10-14 10:51:16 -0400

    Function for Displaying Current Hour
    Add the function for displaying the current hours. Add placeholders for the hours (Saturday Morning Prayer added as example). Add comments for code navigation.

--- littleoffice.py
+++ littleoffice.py
@@ -1,43 +1,471 @@
 import datetime
 
+# 
+# WIP:
+# Need to import all of the missing hours
+# Need to figure out how to add Invitory
+#
+
+# Invitory
+invitory = """
+
+"""
+
+# Sunday Hours
+hourSunMor = """
+
+
+"""
+hourSunEve = """
+
+
+"""
+
+# Monday Hours
+hourMonMor = """
+
+
+"""
+hourMonEve = """
+
+
+"""
+
+# Tuesday Hours
+hourTueMor = """
+
+
+"""
+hourTueEve = """
+
+
+"""
+
+# Wednesday Hours
+hourWedMor = """
+
+
+"""
+hourWedEve = """
+
+"""
+
+# Thursday Hours
+hourThuMor = """
+
+"""
+hourThurEve = """
+
+"""
+
+# Friday Hours
+hourFriMor = """
+
+"""
+hourFriEve = """
+
+"""
+
+# Saturday Hours
+hourSatMor = """
+
+
+    God, come to my assistance.
+    - Lord, make haste to help me
+
+    Glory to the Father and to the Son and to the Holy Spirit
+    as it was in the beginning, is now, and will be forever.  Amen.
+
+
+    HYMN
+
+    Hail, holy Queen of the Heavens. 
+    Hail, holy Queen of the Angels. 
+    Hail, Root of Jesse. 
+    Hail, Gate of Heaven. 
+    By you the Light has entered the world. 
+    Rejoice, glorious Virgin, 
+    Beautiful among all women. 
+    Hail, radiant Splendor, 
+    Intercede with Christ for us.
+ 
+
+    PSALMODY
+
+    Psalm 92
+    
+    Ant.1 Blessed are you, O Mary, for the world's salvation came forth through you; now in glory, you rejoice for ever with the Lord. 
+    
+    Praise of God the Creator
+    Sing in praise of Christ's redeeming work. (St. Athanasius)
+    It is good to give thanks to the Lord,
+    to make music to your name, O Most High,
+    to proclaim your love in the morning
+    and your truth in the watches of the night,
+    on the ten-stringed lyre and the lute,
+    with the murmuring sound of the harp.
+    Your deeds, O Lord, have made me glad;
+    for the work of your hands I shout with joy.
+    O Lord, how great are your works!
+    How deep are your designs!
+    The foolish man cannot know this
+    and the fool cannot understand.
+    Though the wicked spring up like grass
+    and all who do evil thrive,
+    they are doomed to be eternally destroyed.
+    But you, Lord, are eternally on high.
+    See how your enemies perish;
+    all doers of evil are scattered.
+    To me you give the wild ox's strength;
+    you anoint me with the purest oil.
+    My eyes looked in triumph on my foes;
+    my ears heard gladly of their fall.
+    The just will flourish like the palm tree
+    and grow like a Lebanon cedar.
+    Planted in the house of the Lord
+    they will flourish in the courts of our God,
+    still bearing fruit when they are old,
+    still full of sap, still green,
+    to proclaim that the Lord is just.
+    In him, my rock, there is no wrong. 
+
+    Ant.1 Blessed are you, O Mary, for the world's salvation came forth through you; now in glory, you rejoice for ever with the Lord. 
+
+
+    Canticle: Ezekiel 36:24-28
+
+    "The Lord will renew his people
+    They will be his own people, and God himself will be with them, their own God." (Revelation 21:3)
+
+    Ant. 2 The Virgin Mary is exalted above the choirs of angels; let all believers rejoice and bless the Lord.  
+
+    I will take you away from among the nations, 
+    gather you from all the foreign lands, 
+    and bring you back to your own land.
+    I will sprinkle clean water upon you 
+    to cleanse you from all your impurities, 
+    and from all your idols I will cleanse you.
+    I will give you a new heart 
+    and place a new spirit within you, 
+    taking from your bodies your stony hearts 
+    and giving you natural hearts.
+    I will put my spirit within you 
+    and make you live by my statutes, 
+    careful to observe my decrees.
+    You shall live in the land I gave your fathers; 
+    you shall be my people, 
+    and I will be your God.
+
+    Ant. 2 The Virgin Mary is exalted above the choirs of angels; let all believers rejoice and bless the Lord.  
+
+
+    Psalm 8
+    
+    "The majesty of the Lord and man's dignity
+    The Father gave Christ lordship of creation" (Ephesians 1:22)
+
+    Ant. 3: The Lord has made you so glorious that your praise will never cease to resound among men.
+    
+    How great is your name, O Lord our God,
+    through all the earth!
+    Your majesty is praised above the heavens;
+    on the lips of children and of babes
+    you have found praise to foil your enemy,
+    to silence the foe and the rebel.
+    When I see the heavens, the work of your hands,
+    the moon and the stars which you arranged,
+    what is man that you should keep him in mind,
+    mortal man that you care for him?
+    Yet you have made him little less than a god;
+    with glory and honor you crowned him,
+    gave him power over the works of your hands,
+    put all things under his feet.
+    All of them, sheep and cattle,
+    yes, even the savage beasts,
+    birds of the air, and fish
+    that make their way through the waters.
+    How great is your name, O Lord our God
+    through all the earth! 
+
+    Ant. 3: The Lord has made you so glorious that your praise will never cease to resound among men.
+
+
+    FIRST READING: Isaiah 61:10
+
+    I rejoice heartily in the Lord, 
+        in my God is the joy of my soul; 
+
+    For he has clothed me with a robe of salvation, 
+        and wrapped me in a mantle of justice, 
+        like a bride bedecked with her jewels.
+   
+    
+    SECOND READING: From a letter by Paschasius Radbert, abbot (Letter Cogito me 20-23,26,28: PL 30, 122,42)
+
+    Mary is taken from earth
+
+    Today the glorious, ever-virgin Mary ascends to heaven. I urge you to rejoice, for, if I may so put it, she has been raised up in an ineffable way to be with Christ who reigns for ever. The Queen of the world is today taken from the earth and from this present evil time. I say again: rejoice, because she who is sure of her imperishable glory has reached the palace of heaven. 
+    Exalt, I say, and rejoice, and let the whole world rejoice, because this day Salvation has drawn nearer for us all... 
+
+    "Hail, Mary, full of grace; the Lord is with thee; blessed art thou amongst women." It was fitting that the Virgin should be given such gifts and be full of grace, since she has bestowed glory on heaven and has brought God and peace to the earth, faith to pagans, an end to vice, order to life, and discipline to morals. 
+    And it was right that an angel be sent to the Virgin, because virginity always means kinship with the angels... 
+
+    "Rejoice," the angel says, "for you are full of grace." Yes, full! for while a share of grace was given to others, the undiminished fullness of grace was poured into Mary.
+
+
+    RESPONSORY
+
+    The Virgin Mary was taken up to heaven.
+        - The Virgin Mary was taken up to heaven. 
+
+    For all eternity she shares the victory of Christ.
+        - The Virgin Mary was taken up to heaven.
+
+    Glory to the Father and to the Son and to the Holy Spirit, 
+        - The Virgin Mary was taken up to heaven.
+
+
+    CANTICLE OF ZECHARIAH: Luke 1:68-79
+
+    The Messiah and his forerunner
+
+    Ant. This daughter of Jerusalem is lovely and beautiful as she ascends to heaven like the rising sun at daybreak.
+      
+    Blessed be the Lord, the God of Israel; 
+    he has come to his people and set them free.
+ 
+    He has raised up for us a mighty savior, 
+    born of the house of his servant David.
+ 
+    Through his holy prophets he promised of old,
+        that he would save us from our enemies,
+        from the hands of all who hate us.
+ 
+    He promised to show mercy to our fathers
+    and to remember his holy covenant.
+ 
+    This was the oath he swore to our father Abraham,
+    to set us free from the hands of our enemies,
+    free to worship him without fear,
+    holy and righteous in his sight
+        all the days of our life.
+ 
+    You, my child, shall be called the prophet of the Most High,
+    for you will go before the Lord to prepare his way,
+ 
+    To give his people knowledge of salvation
+    by the forgiveness of their sins.
+ 
+    In the tender compassion of our God
+    the dawn from on high shall break upon us,
+    to shine on those who dwell in darkness and the shadow of death,
+    and to guide our feet into the way of peace.
+ 
+    Glory to the Father, and to the Son, and to the Holy Spirit:
+    as it was in the beginning, is now, and will be for ever.  Amen.
+
+    Ant. This daughter of Jerusalem is lovely and beautiful as she ascends to heaven like the rising sun at daybreak.
+
+
+    INTERCESSIONS
+
+    Let us glorify our Savior, who chose the Virgin Mary for his mother. Let us ask him:
+        - May your mother intercede for us, Lord.
+
+    Eternal Word, you chose Mary as the uncorrupted ark of your dwelling place,
+        - Free us from the corruption of sin. 
+
+    You are our redeemer, who made the immaculate Virgin Mary your purest home and the sanctuary of the Holy Spirit,
+        - Make us temples of your Spirit for ever. 
+
+    King of kings, you lifted up your mother, body and soul, into heaven, 
+        - Help us to fix our thoughts on things above. 
+
+    Lord of heaven and earth, you crowned Mary and set her at your right hand as queen,
+        - Make us worthy to share this glory.
+
+
+    Our Father who art in Heaven, 
+    Hallowed be thy name; 
+    Thy kingdom come 
+    Thy will be done 
+    On earth as it is in heaven. 
+    Give us this day our daily bread; 
+    And forgive us our trespasses 
+    As we forgive those who trespass against us; 
+    And lead us not into temptation, 
+    But deliver us from evil. 
+
+
+    CONCLUDING PRAYER
+
+    All-powerful and ever-living God, 
+    you raised the sinless Virgin Mary, 
+    mother of your Son, 
+    body and soul to the glory of heaven. 
+    May we see heaven as our final goal 
+    and come to share her glory. 
+    We ask this through our Lord Jesus Christ, your Son, 
+    who lives and reigns with you and the Holy Spirit, 
+    one God, for ever and ever.
+    May the Lord bless us,
+    protect us from evil
+    and bring us to everlasting life.
+
+        -  Amen.
+"""
+hourSatEve = """
+
+"""
+
+# Office of Readings
+readings = """
+
+"""
+
+# Daytime Prayer
+daytime = """
+
+"""
+
+# Night Prayer
+night = """
+
+"""
+
+# Define the times for the liturgical hours
 def whichHour():
 	hour = ""
 	now = datetime.datetime.now()
 
 	if 4 <= now.hour < 9:
-		hour += "morning"
+		hour += "Morning"
 	elif 9 <= now.hour < 17:
-		hour += "daytime"
+		hour += "Daytime"
 	elif 17 <= now.hour <= 21:
-		hour += "evening"
+		hour += "Evening"
 	else:
-		hour += "night"
+		hour += "Night"
 	return hour
 
+# Define the days for the readings
 def whichDay():
 	day = ""
 	weekday = datetime.datetime.today().weekday()
 
 	if weekday == 0:
-		day += "monday"
+		day += "Monday"
 	elif weekday == 1:
-		day += "tuesday"
+		day += "Tuesday"
 	elif weekday == 2:
-		day += "wednesday"
+		day += "Wednesday"
 	elif weekday == 3:
-		day += "thursday"
+		day += "Thursday"
 	elif weekday == 4:
-		day += "friday"
+		day += "Friday"
 	elif weekday == 5:
-		day += "saturday"
+		day += "Saturday"
 	else:
-		day += "sunday"
+		day += "Sunday"
 	return day
+
 
 day = whichDay()
 hour = whichHour()
 
-print("The current prayer for The Little Office of the Blessed Virgin Mary is " + day + " " + hour)
+# Display the current liturgical hour
+def beginOffice():  
+    if day == "Sunday":
+        if hour == "Morning":
+            print(hourSunMor)
+        elif hour == "Daytime":
+            print(daytime)
+        elif hour == "Evening":
+            print(hourSunEve)
+        elif hour == "Night":
+            print(night)
+        else:
+            print(readings)
 
-# Still need to insert actual hours
+    elif day == "Monday":
+        if hour == "Morning":
+            print(hourMonMor)
+        elif hour == "Daytime":
+            print(daytime)
+        elif hour == "Evening":
+            print(hourMonEve)
+        elif hour == "Night":
+            print(night)
+        else:
+            print(readings)
 
+    elif day == "Tuesday":
+        if hour == "Morning":
+            print(hourTueEve)
+        elif hour == "Daytime":
+            print(daytime)
+        elif hour == "Evening":
+            print(hourTueEve)
+        elif hour == "Night":
+            print(night)
+        else:
+            print(readings)
+
+    elif day == "Wednesday":
+        if hour == "Morning":
+            print(hourWedMor)
+        elif hour == "Daytime":
+            print(daytime)
+        elif hour == "Evening":
+            print(hourWedEve)
+        elif hour == "Night":
+            print(night)
+        else:
+            print(readings)
+
+    elif day == "Thursday":
+        if hour == "Morning":
+            print(hourThuMor)
+        elif hour == "Daytime":
+            print(daytime)
+        elif hour == "Evening":
+            print(hourThurEve)
+        elif hour == "Night":
+            print(night)
+        else:
+            print(readings)
+
+    elif day == "Friday":
+        if hour == "Morning":
+            print(hourFriMor)
+        elif hour == "Daytime":
+            print(daytime)
+        elif hour == "Evening":
+            print(hourFriEve)
+        elif hour == "Night":
+            print(night)
+        else:
+            print(readings)
+
+    elif day == "Saturday":
+        if hour == "Morning":
+            print(hourSatMor)
+        elif hour == "Daytime":
+            print(daytime)
+        elif hour == "Evening":
+            print(hourSatEve)
+        elif hour == "Night":
+            print(night)
+        else:
+            print(readings)
+    
+    else
+        print("error")
+
+
+print("The Little Office of the Blessed Virgin Mary")
+print("")
+print(day + " " + hour + " Prayer")
+print("")
+
+beginOffice()
